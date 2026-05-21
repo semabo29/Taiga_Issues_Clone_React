41c3e6bb31fdf6ea2184b14e6d6713e3d8fed60a
Claudiagalan3 <claudia.galan@estudiantat.upc.edu>
2026-05-21 15:54:48 +0200

    feat: list and add watchers to an issue

--- src/components/UserComments.jsx
+++ src/components/UserComments.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import api from '../api'; // Importa la teva instància d'Axios configurada
+import api from '../api'; 
 import './UserComments.css';
 
 const UserComments = ({ userId }) => {
